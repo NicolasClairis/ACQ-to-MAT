65c00d7f17ebee984732e5274e6afb622d7334de
Samantha Kumarasena <skumarasena@partners.org>
2016-12-20 14:41:18 -0500

    Adding code, gitignore

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ACQ-to-MAT
+
+12/20/2016 Samantha Kumarasena
+
+Modified acq2mat.m to allow user to specify which channels of the ACQ file will be loaded and saved. Made these modifications because of memory issues. 
+
+Renamed load_acq.m to load_acq_chan.m. 
+
+Edited load_acq() and read_acq(), creating new versions of these functions (load_acq_chan() and read_acq_chan() respectively). 
+
+User can modify the "chindices" vector in acq2mat.m to specify the indices (1-indexed) of the channels they would like to save. 
+
+Output: acq struct containing "data", a matrix of doubles containing the data from the specified channels.
---
+++ acq2mat.m
@@ -0,0 +1,56 @@
+
+% 12/20/16, Samantha Kumarasena
+% Modified version of ACQ2MAT: will save only the channels specified by the
+% vector "chindices" below. 
+% 
+% Modify the vector to change the indices of the channels to be saved.
+
+
+% --------------------------------------------------------------------
+%  ACQ2MAT:  This script will automatically convert all ACQ files to MAT
+%  files in all specified folders. You can simply let it run overnight.
+%  The reason to do so is because it could be very time consuming to load
+%  ACQ files, and to load corresponding MAT files will be much quicker.
+%
+%  i.e. In your MATLAB code,
+%	instead of using:  acq = load_acq('MyGSR.acq');
+%	you can now use:   load('MyGSR.mat');
+%
+%  Usage:  Please update the acq_folder list below
+
+
+acq_folder = {pwd};
+%acq_folder = [acq_folder {'C:\GSR\1001'}];	% uncomment it if necessary
+%acq_folder = [acq_folder {'C:\GSR\1002'}];	% uncomment it if necessary
+
+chindices = [1 2];  %SPECIFY CHANNEL(S) TO SAVE
+
+
+%%%%%%%  Please specify all ACQ folders above   %%%%%%%
+
+
+%%%%%%%  Please do not modify any code below   %%%%%%%
+
+for i=1:length(acq_folder)
+   fn_lst = dir(fullfile(acq_folder{i}, '*.acq'));
+
+   for j=1:length(fn_lst)
+      acq_fn = fullfile(acq_folder{i}, fn_lst(j).name);
+      [p f] = fileparts(acq_fn);
+      suffix = ['-Channels-' mat2str(chindices)];
+      mat_fn = fullfile(acq_folder{i}, [f suffix '.mat']);
+
+      if ~exist(mat_fn,'file')
+                            
+          acq = load_acq_chan(acq_fn, chindices);
+
+         v7 = version;
+         if str2num(v7(1))<7
+            save(mat_fn, 'acq');
+         else
+            save(mat_fn, '-V6', 'acq');
+         end
+      end
+   end
+end
+
