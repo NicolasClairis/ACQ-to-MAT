b741ea68e0391671259073401421c776cb6f652a
Samantha Kumarasena <skumarasena@partners.org>
2017-01-10 11:25:56 -0500

    New scripts to find features
    Creating scripts to search for features. Will eventually make new repo?

--- spikes/BM-40/feature_gen.m
+++ spikes/BM-40/feature_gen.m
@@ -1,15 +1,30 @@
 
+
 % Samantha Kumarasena, Jan 2017
+%
 % Looking at possibilities for features distinguishing spikes, seizures, and 
 % artifacts. These possibilities include:
 %    - PSD: ie. periodogram() from the Signal Processing Toolbox
 %    - FFT
-%    - Autocorrelation: ie. autocorr()
+%    - ACF: ie. autocorr()
+%
+% Function inputs: filename, create_plots, save_data
+%    - filename: name of .mat file containing spike/seizure/artifact data to be
+%    analyzed
+%    - create_plots: boolean value indicating whether to generate plots
+%    - save_data: boolean value indicating whether to save return variables
+%    in .mat file
+%
+% Function returns: psd, fourier, auto, bounds
+%    - psd: Three cols, f (freq vector) and pxx (psd, 1 col for each chan)
+%    - fourier: Three cols, f (freq) and P1 (fft, 1 col for each chan)
+%    - auto: Three cols, lags1 (lags), acf1, and acf2 (for each chan)
+%    - bounds: 1x2 vec containing upper/lower bounds for ACF
 
 
+
+function [psd, fourier, auto, bounds] = feature_gen(filename, create_plots, save_data)
 %% Loads data from file
-clear all;
-filename = 'BM40_spk2.mat';
 load(filename);
 
 fs = 500;                           % Sampling frequency of data: 500 Hz
@@ -18,15 +33,19 @@ fs = 500;                           % Sampling frequency of data: 500 Hz
 
 % periodogram(data);                      % plots in dB per unit frequency (normalized)
 
-figure;                                   % plots saved data, in dB, non-normalized freq
-[pxx, f] = periodogram(data, [], [], fs);       
-plot(f, 10*log10(pxx));
-hold on;
-xlabel('Frequency (Hz)');
-ylabel('dB');
-title('Power Spectral Density Estimate');
-legend('Channel 1', 'Channel 2');
-hold off;
+[pxx, f] = periodogram(data, [], [], fs); % calculates PSD
+psd = [f pxx];
+
+if(create_plots)
+    figure;                                   % plots saved data, in dB, non-normalized freq
+    plot(f, 10*log10(pxx));
+    hold on;
+    xlabel('Frequency (Hz)');
+    ylabel('dB');
+    title('Power Spectral Density Estimate');
+    legend('Channel 1', 'Channel 2');
+    hold off;
+end;
 
 
 %% Plots Fourier transform of both channels on the same plot.
@@ -38,40 +57,40 @@ f = fs*(0:(L/2))/L;                     % Frequency vector
 
 Y = fft(data);                          % Calculating FFT
 [row col] = size(Y);
-
 fourier = f';
 
 colors = ['r' 'b' 'c' 'm' 'k'];
 
-figure;
-% For each channel, obtain 1-side spectrum from double-sided
-for i = 1:col
-    
-    ch = Y(:,i);                        % Single channel data
-   
-    P2 = abs(ch/L);                     % Two-sided spectrum                      
-    P1 = P2(1:L/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-    
-    colindex = mod(i, length(colors));
-    color = colors(colindex);
-    
-    plot(f, P1, 'color', color);        % Plot channel FFT in random color
-    
-    fourier = [fourier P1];
-    
-    hold on;
-   
+if(create_plots)
+    figure;
+    % For each channel, obtain 1-side spectrum from double-sided
+    for i = 1:col
+
+        ch = Y(:,i);                        % Single channel data
+
+        P2 = abs(ch/L);                     % Two-sided spectrum                      
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+
+        colindex = mod(i, length(colors));  % generating color for plot
+        color = colors(colindex);
+
+        plot(f, P1, 'color', color);        % Plot channel FFT in random color
+        fourier = [fourier P1];             % save new FFT data in return variable
+
+        hold on;
+    end;
+
+    xlabel('Frequency (Hz)');               %legend
+    ylabel('FFT');
+    title('FFT Plot of Data');
+    legend('Channel 1', 'Channel 2');
+    hold off;
+
 end;
 
-xlabel('Frequency (Hz)');
-ylabel('FFT');
-title('FFT Plot of Data');
-legend('Channel 1', 'Channel 2');
-hold off;
 
-
-%% Calculating and plotting autocorrelation
+%% Calculating and plotting autocorrelation (ACF)
 
 %{
 
@@ -93,8 +112,11 @@ hold off;
 [acf1, lags1, bounds1] = autocorr(data(:,1), 100);
 [acf2, lags2, bounds2] = autocorr(data(:,2), 100);
 
+auto = [lags1 acf1 acf2];
+bounds = bounds1;
+
 %plotting autocorr data
-if(lags1 == lags2)                       %if... then can plot both autocorrs on the same plot
+if(create_plots)
     figure;
     stem(lags1, acf1, 'r');              % plot first channel data
     hold on;
@@ -113,10 +135,10 @@ if(lags1 == lags2)                       %if... then can plot both autocorrs on 
 end
 
 %% Save relevant PSD, FFT, and autocorr variables
-savefile = strcat(filename, '-feat.mat');
-save(savefile, 'psd', 'fourier', 'auto', 'bounds');
 
+if(save_data)
+    savefile = strcat(filename(1:(end-4)), '-feat.mat');
+    save(savefile, 'psd', 'fourier', 'auto', 'bounds');
+end;
 
-
-
-
+end
